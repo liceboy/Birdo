b43683882c56a9a4100e92c6033f35e136b1b6b1
bjguan <bjguan@PLD-LAB-400-17T.fayette.ketsds.net>
2019-09-10 11:30:22 -0400

    work

--- birdo/src/birdo/title.java
+++ birdo/src/birdo/title.java
@@ -11,10 +11,8 @@ public class title {
 	
 	public void draw(Graphics g) {
 		g.setColor(Color.BLACK);
-		g.drawString("This is the title", 100, 100);
-		g.drawString("Start Game with 2 player: Space", 100, 130);
-		g.drawString("Start game with 1 player: Z", 100, 160);
-		g.drawString("Start Game1: Shift", 100, 190);
+		g.drawString("birdo", 100, 100);
+		g.drawString("Start Game: Space", 100, 130);
 	}
 	
 	public void move() {
