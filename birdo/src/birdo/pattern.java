24e82930e3e65abb391289b5191d13deaf98b07c
Ben Guan <Ben Guan@DESKTOP-0SRTM8H>
2019-10-02 21:09:28 -0400

    wtf

---
+++ birdo/src/birdo/pattern.java
@@ -0,0 +1,14 @@
+package birdo;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class pattern {
+	InputStream in = getClass().getResourceAsStream("src/birdo/patterns"); 
+	Scanner sc = new Scanner(in);
+	String patternname;
+	 public pattern (String pn) {
+		 pn = patternname;
+	 }
+}
+
